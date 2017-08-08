54276906fdf170dccf265c0fa95333f8b9988ca8
SebaDro <sebastian.drost@hs-bochum.de>
2017-08-08 15:03:03 +0200

    add O&M observation decoding for MQTT

---
+++ extensions/mqtt/src/main/java/org/n52/sos/mqtt/convert/OmInsertObservationConverter.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (C) 2017 52north.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+package org.n52.sos.mqtt.convert;
+
+import com.google.common.collect.Lists;
+import java.util.List;
+import org.n52.sos.decode.json.impl.InsertObservationRequestDecoder;
+import org.n52.sos.decode.json.impl.ObservationDecoder;
+import org.n52.sos.mqtt.api.OmMessage;
+import org.n52.sos.ogc.om.OmObservation;
+import org.n52.sos.ogc.ows.OwsExceptionReport;
+import org.n52.sos.ogc.sos.Sos2Constants;
+import org.n52.sos.ogc.sos.SosConstants;
+import org.n52.sos.request.InsertObservationRequest;
+import org.n52.sos.request.RequestContext;
+import org.n52.sos.util.net.IPAddress;
+
+/**
+ *
+ * @author Sebastian Drost
+ */
+public class OmInsertObservationConverter implements MqttInsertObservationConverter<OmMessage> {
+
+    private ObservationDecoder observationDecoder;
+
+    public OmInsertObservationConverter() {
+        observationDecoder = new ObservationDecoder();
+    }
+
+    @Override
+    public InsertObservationRequest convert(OmMessage message) throws OwsExceptionReport {
+        List<OmObservation> observations = observationDecoder.decodeJsonToObjectList(message.getOmPayload(), OmObservation.class);
+
+        InsertObservationRequest request = new InsertObservationRequest();
+        request.setService(SosConstants.SOS);
+        request.setVersion(Sos2Constants.SERVICEVERSION);
+        RequestContext requestContext = new RequestContext();
+        requestContext.setIPAddress(new IPAddress("127.0.0.1"));
+        request.setRequestContext(requestContext);
+        request.setOfferings(Lists.newArrayList(message.getProcedure()));
+        request.setObservation(observations);
+        return request;
+    }
+
+}
---
+++ extensions/mqtt/src/main/java/org/n52/sos/mqtt/decode/OmDecoder.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright (C) 2017 52north.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+package org.n52.sos.mqtt.decode;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonToken;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.n52.sos.config.annotation.Configurable;
+import org.n52.sos.config.annotation.Setting;
+import org.n52.sos.mqtt.MqttSettings;
+import org.n52.sos.mqtt.api.MqttMessage;
+import org.n52.sos.mqtt.api.OmMessage;
+import org.n52.sos.mqtt.convert.MqttInsertObservationConverter;
+import org.n52.sos.mqtt.convert.MqttInsertSensorConverter;
+import org.n52.sos.mqtt.convert.OmInsertObservationConverter;
+import org.n52.sos.mqtt.convert.OmInsertSensorConverter;
+
+/**
+ *
+ * @author Sebastian Drost
+ */
+public class OmDecoder extends AbstractMqttDecoder {
+
+    private String[] observableProperty;
+    private String observationField;
+
+    @Override
+    protected MqttMessage parseMessage(JsonNode n) {
+        JsonNode payload = n.findParent(observationField).get(observationField);
+        OmMessage message = new OmMessage();
+        if (payload.isArray()) {
+            message.setProcedure(payload.get(0).get("procedure").asText());
+        } else {
+            message.setProcedure(payload.get("procedure").asText());
+        }
+        message.setOmPayload(payload);
+        return message;
+    }
+
+    @Override
+    public MqttInsertSensorConverter getInsertSensorConverter() {
+        OmInsertSensorConverter converter = new OmInsertSensorConverter();
+        converter.setObservableProperty(observableProperty);
+        return converter;
+    }
+
+    @Override
+    public MqttInsertObservationConverter getInsertOnbservationConverter() {
+        return new OmInsertObservationConverter();
+    }
+
+    public String[] getObservableProperty() {
+        return observableProperty;
+    }
+
+    @Setting(MqttSettings.MQTT_OM_OBSERVABLE_PROPERTY)
+    public void setObservableProperty(String observableProperty) {
+        this.observableProperty = observableProperty.split(";");
+    }
+
+    public String getObservationField() {
+        return observationField;
+    }
+
+    @Setting(MqttSettings.MQTT_OM_OBSERVATION_FIELD)
+    public void setObservationField(String observationField) {
+        this.observationField = observationField;
+    }
+
+}
