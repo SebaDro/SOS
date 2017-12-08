f898e3a54ebb7a120abc70b124af052c58137197
SebaDro <sebastian.drost@hs-bochum.de>
2017-12-08 09:22:33 +0100

    Fix O&M JSON observation decoder

--- extensions/mqtt/src/main/java/org/n52/sos/mqtt/convert/OmInsertObservationConverter.java
+++ extensions/mqtt/src/main/java/org/n52/sos/mqtt/convert/OmInsertObservationConverter.java
@@ -28,9 +28,12 @@
  */
 package org.n52.sos.mqtt.convert;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.google.common.collect.Lists;
-import javax.inject.Inject;
+import java.util.List;
 import org.n52.janmayen.net.IPAddress;
+import org.n52.shetland.ogc.om.OmObservation;
 import org.n52.shetland.ogc.ows.exception.NoApplicableCodeException;
 import org.n52.shetland.ogc.ows.exception.OwsExceptionReport;
 import org.n52.shetland.ogc.ows.service.OwsServiceRequestContext;
@@ -39,7 +42,7 @@ import org.n52.shetland.ogc.sos.SosConstants;
 import org.n52.shetland.ogc.sos.request.InsertObservationRequest;
 import org.n52.sos.mqtt.api.OmMessage;
 import org.n52.svalbard.decode.exception.DecodingException;
-import org.n52.svalbard.decode.json.InsertObservationRequestDecoder;
+import org.n52.svalbard.decode.json.ObservationDecoder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -51,30 +54,47 @@ public class OmInsertObservationConverter implements MqttInsertObservationConver
 
     private static final Logger LOG = LoggerFactory.getLogger(OmInsertObservationConverter.class);
 
-    @Inject
-    InsertObservationRequestDecoder insertObservationDecoder;
+    private ObservationDecoder observationDecoder;
 
-//    public OmInsertObservationConverter() {
-//        insertObservationDecoder = new InsertObservationRequestDecoder();
-//    }
+    public OmInsertObservationConverter() {
+        observationDecoder = new ObservationDecoder();
+    }
 
     @Override
     public InsertObservationRequest convert(OmMessage message) throws OwsExceptionReport {
-//        List<OmObservation> observations = observationDecoder.decodeJsonToObjectList(message.getOmPayload(), OmObservation.class);
-
-        InsertObservationRequest request;
         try {
-            request = insertObservationDecoder.decodeJSON(message.getOmPayload(), false);
+            InsertObservationRequest request = new InsertObservationRequest();
             request.setService(SosConstants.SOS);
             request.setVersion(Sos2Constants.SERVICEVERSION);
+
             OwsServiceRequestContext requestContext = new OwsServiceRequestContext();
             requestContext.setIPAddress(new IPAddress("127.0.0.1"));
             request.setRequestContext(requestContext);
+
+            List<OmObservation> observations = createObservations(message.getOmPayload());
             request.setOfferings(Lists.newArrayList(message.getProcedure()));
+            request.setObservation(observations);
+
             return request;
         } catch (DecodingException ex) {
             throw new NoApplicableCodeException().causedBy(ex);
         }
+    }
+
+    private List<OmObservation> createObservations(JsonNode observationNode) throws DecodingException {
+        List<OmObservation> observations = Lists.newArrayList();
+        if (observationNode.isArray()) {
+            ((ArrayNode) observationNode).forEach(n -> {
+                try {
+                    observations.add(observationDecoder.decode(n));
+                } catch (DecodingException ex) {
+                    LOG.error("Error while parsing observations.", ex);
+                }
+            });
+        } else {
+            observations.add(observationDecoder.decode(observationNode));
+        }
+        return observations;
     }
 
 }
