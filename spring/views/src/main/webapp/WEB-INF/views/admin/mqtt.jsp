f875e4719b192d121faaf27f275fa1780e475b39
SebaDro <sebastian.drost@hs-bochum.de>
2018-01-11 17:51:10 +0100

    Introduce view and javascript controller for MQTT configuration

---
+++ spring/views/src/main/webapp/WEB-INF/views/admin/mqtt.jsp
@@ -0,0 +1,184 @@
+<%--
+
+    Copyright (C) 2012-2018 52°North Initiative for Geospatial Open Source
+    Software GmbH
+
+    This program is free software; you can redistribute it and/or modify it
+    under the terms of the GNU General Public License version 2 as published
+    by the Free Software Foundation.
+
+    If the program is linked with libraries which are licensed under one of
+    the following licenses, the combination of the program with the linked
+    library is not considered a "derivative work" of the program:
+
+        - Apache License, version 2.0
+        - Apache Software License, version 1.0
+        - GNU Lesser General Public License, version 3
+        - Mozilla Public License, versions 1.0, 1.1 and 2.0
+        - Common Development and Distribution License (CDDL), version 1.0
+
+    Therefore the distribution of the program linked with libraries licensed
+    under the aforementioned licenses, is permitted by the copyright holders
+    if the distribution is compliant with both the GNU General Public
+    License version 2 and the aforementioned licenses.
+
+    This program is distributed in the hope that it will be useful, but
+    WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+    Public License for more details.
+
+--%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ taglib prefix="fn" uri="http://java.sun.com/jsp/jstl/functions"%>
+<jsp:include page="../common/header.jsp">
+    <jsp:param name="activeMenu" value="admin" />
+</jsp:include>
+
+<link rel="stylesheet" href="<c:url value="/static/lib/prettify.css" />" type="text/css" />
+<link rel="stylesheet" href="<c:url value="/static/lib/codemirror-2.34.css" />" type="text/css" />
+<link rel="stylesheet" href="<c:url value="/static/css/codemirror.custom.css" />" type="text/css" />
+<link rel="stylesheet" href="<c:url value="/static/lib/bootstrap-toggle-buttons.css" />" type="text/css" />
+<script type="text/javascript" src="<c:url value="/static/lib/codemirror-2.34.js" />"></script>
+<script type="text/javascript" src="<c:url value="/static/lib/codemirror-2.34-xml.js" />"></script>
+<script type="text/javascript" src="<c:url value="/static/lib/prettify.min.js" />"></script>
+<script type="text/javascript" src="<c:url value="/static/lib/vkbeautify-0.99.00.beta.js" />"></script>
+<script type="text/javascript" src="<c:url value="/static/lib/jquery.toggle.buttons.js" />"></script>
+
+<script type="text/javascript" src="<c:url value="/static/js/jquery.additions.js" />"></script>
+<script type="text/javascript" src="<c:url value="/static/js/EventMixin.js" />"></script>
+<script type="text/javascript" src="<c:url value="/static/js/MqttConfigurationController.js" />"></script>
+
+<jsp:include page="../common/logotitle.jsp">
+    <jsp:param name="title" value="MQTT Setting" />
+    <jsp:param name="leadParagraph" value="Use this site to define your MQTT settings." />
+</jsp:include>
+
+<style type="text/css">
+    .btn-icon { height: 30px; }
+    .btn-icon i { margin-right: 0px !important; }
+    .btn-single { margin-bottom: 1px;}
+    #stcaps-publish { margin-bottom: -11px; }
+    #back-top {
+        position: fixed;
+        bottom: 30px;
+        margin-left: -150px;
+    }
+    #back-top a {
+        width: 108px;
+        display: block;
+        text-align: center;
+        font: 11px Arial, Helvetica, sans-serif;
+        text-transform: uppercase;
+        text-decoration: none;
+        color: #bbb;
+        /* background color transition */
+        -webkit-transition: 1s;
+        -moz-transition: 1s;
+        transition: 1s;
+    }
+    #back-top a:hover {
+        color: #000;
+    }
+    #back-top i {
+        display: block;
+        margin-bottom: 7px;
+        margin-left: 48px;
+        -webkit-border-radius: 15px;
+        -moz-border-radius: 15px;
+        border-radius: 15px;
+        -webkit-transition: 1s;
+        -moz-transition: 1s;
+        transition: 1s;
+    }
+</style>
+
+<div id="mqtt-configuration-container" class="column">
+    <div class="row">
+        <div class="span12 form-inline" style="margin-bottom: 5px;">
+            <select id="mqttconf-list" class="span6">
+                <c:forEach var="config" items="${mqttConfigurations}">
+                    <option value="${config.key}">${config.name}</option>
+                </c:forEach> 
+            </select>
+            <div class="btn-group">
+                <button id="mqttconf-addnew-button" title="Add new MQTT configuration" type="button" class="btn btn-icon mqttconf-edit-button"><i class="icon-plus"></i></button>
+            </div>
+            <div class="btn-group pull-right">
+                <button id="mqttconf-activate-button" title="Activate" type="button" class="btn btn-danger">Active</button>
+            </div>
+            <div id="mqttconf-add-new-form" class="input-append input-prepend control-group" style="display: none;">
+                <input class="input-xlarge" id="mqttconf-new-name-input" type="text" placeholder="Identifier"/>
+                <div class="btn-group">
+                    <button type="button" title="Add" class="btn btn-icon mqttconf-add-new-form-button" id="mqttconf-add-new-form-ok"><i class="icon-ok"></i></button>
+                    <button type="button" title="Dismiss" class="btn btn-icon mqttconf-add-new-form-button" id="mqttconf-add-new-form-cancel"><i class="icon-remove"></i></button>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+    <div id="mqtt-configuration-properties-container" class="column">
+        <legend >
+            <div class="row">
+                <div>Configuration for MQTT client </div>
+                <div>Test</div>
+            </div>
+        </legend>
+        <div class="control-group">
+            <label class="control-label" for="mqttconf-name-input">MQTT configuration name</label>
+            <div class="controls">
+                <input type="text" class="input-xlarge" id="mqttconf-name-input" value="${selectedMqttConfiguration.name}">
+            </div>
+        </div>
+        <div class="control-group">
+            <label class="control-label" for="mqttconf-host-input">MQTT broker host</label>
+            <div class="controls">
+                <input type="text" class="input-xlarge" id="mqttconf-host-input" value="${host}" />
+            </div>
+        </div>
+        <div class="control-group">
+            <label class="control-label" for="mqttconf-port-input">MQTT broker port</label>
+            <div class="controls">
+                <input type="text" class="input-xlarge"  id="mqttconf-port-input" value="${port}" />
+            </div>
+        </div>
+        <div class="control-group">
+            <label class="control-label" for="mqttconf-topic-input">MMQTT broker topic</label>
+            <div class="controls">
+                <input type="text" class="input-xlarge" id="mqttconf-topic-input" value="${selectedMqttConfiguration.topic}" />
+            </div>
+        </div>
+        <div class="control-group">
+            <label class="control-label" for="mqttconf-protocol-select">MQTT protocol</label>
+            <div class="controls">
+                <select id="mqttconf-protocol-select" class="input-xlarge">
+                    <option selected>tcp</option>
+                    <option>ws</option>
+                </select>
+                <span class="help-block">Select the protocol</span>
+            </div>
+        </div>
+        <div class="control-group">
+            <label class="control-label" for="mqttconf-decoder-select">MQTT decoder</label>
+            <div class="controls">
+                <select id="mqttconf-decoder-select" class="input-xlarge">
+                    <option value="org.n52.sos.decode.AdsbDecoder" selected>AdsbDecoder</option>
+                    <option value="org.n52.sos.decode.CtdDecoder" >CtdDecoder</option>
+                    <option value="org.n52.sos.decode.FluorometerDecoder" >FluorometerDecoder</option>
+                    <option value="org.n52.sos.decode.OmDecoder" >OmDecoder</option>
+                    <option value="org.n52.sos.decode.TtnDecoder" >TtnDecoder</option>
+                </select>
+                <span class="help-block">Select the decoder</span>
+            </div>
+        </div>
+        <div class="form-actions">
+            <button id="mqttconf-save-button" class="btn btn-info">Save</button>
+        </div>
+    </div>
+</div>
+<script type="text/javascript">
+    var baseUrl = "<c:url value="/"/>";
+    new MqttConfigurationController(baseUrl);
+</script>
+
+<jsp:include page="../common/footer.jsp" />
---
+++ spring/views/src/main/webapp/static/js/MqttConfigurationController.js
@@ -0,0 +1,254 @@
+/*
+ * Copyright (C) 2012-2018 52°North Initiative for Geospatial Open Source
+ * Software GmbH
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as published
+ * by the Free Software Foundation.
+ *
+ * If the program is linked with libraries which are licensed under one of
+ * the following licenses, the combination of the program with the linked
+ * library is not considered a "derivative work" of the program:
+ *
+ *     - Apache License, version 2.0
+ *     - Apache Software License, version 1.0
+ *     - GNU Lesser General Public License, version 3
+ *     - Mozilla Public License, versions 1.0, 1.1 and 2.0
+ *     - Common Development and Distribution License (CDDL), version 1.0
+ *
+ * Therefore the distribution of the program linked with libraries licensed
+ * under the aforementioned licenses, is permitted by the copyright holders
+ * if the distribution is compliant with both the GNU General Public
+ * License version 2 and the aforementioned licenses.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details.
+ */
+!function ($) {
+
+    function MqttConfigurationController(baseUrl) {
+        var self = this;
+
+        this.selectedConfig = {
+            key: null,
+            active: false,
+            name: "",
+            host: "",
+            port: "",
+            topic: "",
+            protocol: "",
+            username: "",
+            password: "",
+            decoder: "",
+            observableProperties: new Array(),
+            observationField: "",
+            csvLineSeperator: "",
+            csvFieldSeperator: ""
+        };
+
+        this.baseUrl = baseUrl;
+        this.$configList = $("#mqttconf-list");
+
+        this.$editButtons = $(".mqttconf-edit-button");
+        this.$addNewButton = $("#mqttconf-addnew-button");
+        this.$addNewOk = $("#mqttconf-add-new-form-ok");
+        this.$addNewForm = $("#mqttconf-add-new-form");
+        this.$saveButton = $("#mqttconf-save-button");
+        this.$mqttConfPropsContainer = $("#mqtt-configuration-properties-container");
+
+        this.$mqttNewName = $("#mqttconf-new-name-input");
+
+        this.$mqttActivate = $("#mqttconf-activate-button");
+        this.$mqttName = $("#mqttconf-name-input");
+        this.$mqttHost = $("#mqttconf-host-input");
+        this.$mqttPort = $("#mqttconf-port-input");
+        this.$mqttTopic = $("#mqttconf-topic-input");
+        this.$mqttProtocol = $("#mqttconf-protocol-select");
+        this.$mqttDecoder = $("#mqttconf-decoder-select");
+
+        this.bind();
+
+    }
+    $.extend(MqttConfigurationController.prototype, EventMixin);
+    $.extend(MqttConfigurationController.prototype, {
+
+        bind: function () {
+            var self = this;
+            var selectedConfig = self.$configList.find("option:selected").val();
+            if (!self.$configList.val()) {
+                self.$mqttConfPropsContainer.hide();
+            } else {
+                self.requestConfig(selectedConfig);
+            }
+
+            this.$addNewButton.on("click", function () {
+                self.$addNewButton.slideLeft(100, function () {
+                    self.$addNewForm.slideRight();
+                });
+            });
+
+            //show input for adding a new config
+            $(".mqttconf-add-new-form-button").on("click", function () {
+                self.$addNewForm.slideLeft(400, function () {
+                    self.$addNewButton.slideRight(100);
+                    self.$mqttNewName.val("").trigger("input");
+                });
+            });
+
+            //prepare form for editting properties for a new config
+            this.$addNewOk.on("click", function () {
+                self.$mqttName.val(self.$mqttNewName.val());
+                self.selectedConfig.name = self.$mqttNewName.val();
+                self.cleanProperties();
+                self.$mqttConfPropsContainer.slideDown("slow", function () {
+
+                });
+            });
+
+            this.$configList.change(function () {
+                var selectedConfig = self.$configList.find("option:selected");
+                console.log(selectedConfig.val());
+                self.requestConfig(selectedConfig.val());
+            });
+
+            this.$saveButton.on("click", function () {
+                self.readProperties();
+
+                if (self.selectedConfig.key) {
+                    self.update(self.selectedConfig);
+                } else {
+                    self.create(self.selectedConfig);
+                }
+
+            });
+
+            this.$mqttActivate.on("click", function () {
+                var payload = {
+                    mqttConfigurationKey: self.selectedConfig.key,
+                    mqttConfigurationActivation: !self.selectedConfig.active
+                };
+                self.activateConfig(payload);
+            });
+
+        },
+        create: function (payload) {
+            var self = this;
+            $.ajax({
+                type: "POST",
+                url: self.baseUrl + "admin/mqtt",
+                contentType: "application/json",
+                data: JSON.stringify(payload),
+                context: this
+            }).done(function (data) {
+                showSuccess("MQTT configuration for " + data.name + " saved.");
+                self.selectedConfig = data;
+                self.$configList.append($('<option>', {
+                    value: data.key,
+                    text: data.name,
+                    selected: true
+                }));
+
+            }).fail(function () {
+                showError("MQTT configuration for " + payload.name + " could not be saved.");
+            });
+        },
+
+        update: function (payload) {
+            var self = this;
+            $.ajax({
+                type: "PUT",
+                url: self.baseUrl + "admin/mqtt",
+                contentType: "application/json",
+                data: JSON.stringify(payload),
+                context: this
+            }).done(function (data) {
+                showSuccess("MQTT configuration for " + data.name + " updated.");
+                self.$configList.find("option:selected").text(self.selectedConfig.name);
+
+            }).fail(function () {
+                showError("MQTT configuration for " + payload.name + " could not be updated.");
+            });
+        },
+
+        activateConfig: function (payload) {
+            var self = this;
+            self.$mqttActivate.prop("disabled", true);
+            $.ajax({
+                type: "POST",
+                url: self.baseUrl + "admin/mqtt/operations",
+                contentType: "application/json",
+                data: JSON.stringify(payload),
+                context: this
+            }).done(function () {
+                showSuccess("MQTT client " + (this.selectedConfig.active ? "activated" : "deactivated"));
+                self.selectedConfig.active = !self.selectedConfig.active;
+                self.$mqttActivate.toggleClass("btn-danger btn-success")
+                        .text(self.selectedConfig.active ? "active" : "inactive")
+                        .prop("disabled", false);
+//                self.selectedConfig = data;
+//                self.$configList.append($('<option>', {
+//                    value: data.key,
+//                    text: data.name,
+//                    selected: true
+//                }));
+
+            }).fail(function () {
+                showError("MQTT client could not be " + (this.selectedConfig.active ? "activated" : "deactivated"));
+                self.$mqttActivate.prop("disabled", false);
+            });
+        },
+
+        requestConfig: function (id) {
+            var self = this;
+            $.get(self.baseUrl + "admin/mqtt/" + id)
+                    .done(function (data) {
+                        console.log("Data loaded: " + data);
+                        self.selectedConfig = data;
+                        self.updateProperties(data);
+                    }).fail(function () {
+                showError("Data could not be loaded: ");
+            });
+        },
+
+        readProperties: function () {
+            this.selectedConfig.name = this.$mqttName.val();
+            this.selectedConfig.host = this.$mqttHost.val();
+            this.selectedConfig.port = this.$mqttPort.val();
+            this.selectedConfig.topic = this.$mqttTopic.val();
+            this.selectedConfig.protocol = this.$mqttProtocol.val();
+            this.selectedConfig.decoder = this.$mqttDecoder.val();
+        },
+
+        updateProperties: function (data) {
+            this.$mqttName.val(data.name);
+            this.$mqttActivate.removeClass("btn-success btn-danger");
+            this.$mqttActivate.addClass(this.selectedConfig.active ? "btn-success" : "btn-danger");
+            this.$mqttActivate.text(this.selectedConfig.active ? "active" : "inactive");
+            this.$mqttHost.val(data.host);
+            this.$mqttPort.val(data.port);
+            this.$mqttTopic.val(data.topic);
+            this.$mqttProtocol.val(data.protocol);
+            this.$mqttDecoder.val(data.decoder);
+        },
+
+        cleanProperties: function () {
+            this.selectedConfig.key = null;
+            this.selectedConfig.active = false;
+            this.selectedConfig.host = "";
+            this.$mqttHost.val("");
+            this.selectedConfig.port = "";
+            this.$mqttPort.val("");
+            this.selectedConfig.topic = "";
+            this.$mqttTopic.val("");
+            this.selectedConfig.protocol = "tcp";
+            this.$mqttProtocol.val("");
+            this.selectedConfig.decoder = "org.n52.sos.mqtt.decode.AdsbDecoder";
+            this.$mqttDecoder.val("");
+        }
+
+    });
+    window.MqttConfigurationController = MqttConfigurationController;
+
+}(jQuery);
