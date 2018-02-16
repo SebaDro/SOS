d1959a381cbae5c01191dbcef96e630f97fa6290
SebaDro <sebastian.drost@hs-bochum.de>
2018-02-16 10:47:42 +0100

    Adjustments for multiple MQTT cosumer support

--- extensions/mqtt/src/main/java/org/n52/sos/mqtt/MqttConsumer.java
+++ extensions/mqtt/src/main/java/org/n52/sos/mqtt/MqttConsumer.java
@@ -28,24 +28,16 @@
  */
 package org.n52.sos.mqtt;
 
-import java.util.Optional;
 import java.util.UUID;
-import javax.inject.Inject;
-
-import org.n52.faroe.SettingsService;
 import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 import org.n52.faroe.ConfigurationError;
 import org.n52.faroe.annotation.Configurable;
-import org.n52.faroe.annotation.Setting;
 import org.n52.janmayen.Debouncer;
-import org.n52.janmayen.lifecycle.Constructable;
-import org.n52.janmayen.lifecycle.Destroyable;
+import org.n52.sos.mqtt.config.MqttConfiguration;
 import org.n52.sos.mqtt.decode.MqttDecoder;
-import org.n52.sos.mqtt.decode.MqttDecoderRepository;
-import org.n52.svalbard.encode.EncoderRepository;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -54,62 +46,17 @@ import org.slf4j.LoggerFactory;
  *
  * @author Matthes Rieke <m.rieke@52north.org>
  */
-@Configurable
-public class MqttConsumer implements Constructable, Destroyable {
+public class MqttConsumer {
 
     private static final Logger LOG = LoggerFactory.getLogger(MqttConsumer.class);
-    private static MqttConsumer instance;
 
-    private String topic;
-    private String host;
-    private String port;
     private MqttClient client;
-    private String decoder;
-    private String protocol;
-    private String username;
-    private String password;
-    private boolean isActive;
-
+    private MqttConfiguration config;
+    private MqttDecoder decoder;
     private Debouncer debouncer;
 
-    @Inject
-    MqttDecoderRepository mqttDecoderRepository;
-
-    @Inject
-    SettingsService settingsManager;
-
-    @Inject
-    EncoderRepository encoderRepository;
-
-    @Override
-    public void init() {
-        LOG.debug("MQTT client initialized");
-        createDebouncer();
-        try {
-            connect();
-            LOG.info("MQTT client connected");
-        } catch (Exception ex) {
-            LOG.error("Error during connecting MQTT client", ex);
-        }
-    }
-
-    @Override
-    public void destroy() {
-        cleanup();
-    }
-
-    private void createDebouncer() {
-        debouncer = new Debouncer(1000, new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    cleanup();
-                    connect();
-                } catch (Exception ex) {
-                    LOG.error("Error during connecting MQTT client", ex);
-                }
-            }
-        });
+    public MqttConsumer(MqttConfiguration config) {
+        this.config = config;
     }
 
     /**
@@ -119,49 +66,35 @@ public class MqttConsumer implements Constructable, Destroyable {
         AT_MOST_ONCE, AT_LEAST_ONCE, EXACTLY_ONCE
     }
 
-    public static MqttConsumer getInstance() {
-        if (instance == null) {
-            instance = new MqttConsumer();
-//            SettingsManager.getInstance().configure(instance);
-        }
-        return instance;
-    }
-
     /**
      * connects the client
      *
      * @throws MqttException
      */
     public void connect() throws MqttException {
-        if (isActive) {
-            if (client == null) {
-                this.client = new MqttClient(String.format("tcp://%s:%s", getHost(), getPort()), getId(), new MemoryPersistence());
-                LOG.debug("MQTT client created!");
-            }
-            if (!client.isConnected()) {
-                MqttConnectOptions options = new MqttConnectOptions();
-                options.setAutomaticReconnect(true);
-                options.setConnectionTimeout(0);
-                if (username != null && !username.isEmpty() && password != null && !password.isEmpty()) {
-                    options.setUserName(username);
-                    options.setPassword(password.toCharArray());
-                }
-                client.connect(options);
-                LOG.debug("Connected to: {}", String.format("tcp://%s:%s", getHost(), getPort()));
-                try {
-                    Optional<MqttDecoder> decoder = mqttDecoderRepository.getDecoder(getDecoder());
-                    if (!decoder.isPresent()) {
-                        throw new ClassNotFoundException("Decoder is not available");
-                    }
-                    settingsManager.configure(decoder.get());
-                    client.setCallback(new SosMqttCallback(decoder.get()));
-                } catch (InstantiationException | IllegalAccessException | ClassNotFoundException e) {
-                    throw new ConfigurationError("Error while starting MQTT consumer", e);
-                }
-            }
-            subscribe(getTopic(), QualityOfService.EXACTLY_ONCE);
-            LOG.debug("Subscibed to topic: {}", getTopic());
+        if (client == null) {
+            this.client = new MqttClient(String.format("tcp://%s:%s", config.getHost(), config.getPort()), getId(), new MemoryPersistence());
+            LOG.debug("MQTT client created!");
         }
+        if (!client.isConnected()) {
+            MqttConnectOptions options = new MqttConnectOptions();
+            options.setCleanSession(true);
+            options.setAutomaticReconnect(true);
+            options.setConnectionTimeout(0);
+            if (config.getUsername() != null && !config.getUsername().isEmpty() && config.getPassword() != null && !config.getPassword().isEmpty()) {
+                options.setUserName(config.getPassword());
+                options.setPassword(config.getPassword().toCharArray());
+            }
+            client.connect(options);
+            LOG.debug("Connected to: {}", String.format("tcp://%s:%s", config.getHost(), config.getPort()));
+            try {
+                client.setCallback(new SosMqttCallback(decoder));
+            } catch (InstantiationException | IllegalAccessException | ClassNotFoundException e) {
+                throw new ConfigurationError("Error while starting MQTT consumer", e);
+            }
+        }
+        subscribe(config.getTopic(), QualityOfService.AT_MOST_ONCE);
+        LOG.debug("Subscibed to topic: {}", config.getTopic());
     }
 
     public boolean isConnected() {
@@ -183,14 +116,10 @@ public class MqttConsumer implements Constructable, Destroyable {
         client.subscribe(topic, qos.ordinal());
     }
 
-    private void cleanup() {
-        try {
-            if (client != null && client.isConnected() && getTopic() != null) {
-                client.unsubscribe(getTopic());
-                client.disconnect();
-            }
-        } catch (MqttException e) {
-            LOG.error("Error while closing connection!", e);
+    public void cleanup() throws MqttException {
+        if (client != null && client.isConnected() && config.getTopic() != null) {
+            client.unsubscribe(config.getTopic());
+            client.disconnect();
         }
     }
 
@@ -200,102 +129,19 @@ public class MqttConsumer implements Constructable, Destroyable {
         }
     }
 
+    public MqttConfiguration getConfig() {
+        return this.config;
+    }
+
     private String getId() {
         return UUID.randomUUID().toString();
     }
 
-    @Setting(MqttSettings.MQTT_TOPIC)
-    public void setTopic(String topic) {
-        this.topic = topic;
-        requestConnecting();
-    }
-
-    @Setting(MqttSettings.MQTT_HOST)
-    public void setHost(String host) {
-        this.host = host;
-        requestConnecting();
-    }
-
-    @Setting(MqttSettings.MQTT_PORT)
-    public void setPort(String port) {
-        this.port = port;
-        requestConnecting();
-    }
-
-    @Setting(MqttSettings.MQTT_DECODER)
-    public void setDecoder(String decoder) {
-        this.decoder = decoder;
-        requestConnecting();
-    }
-
-    @Setting(MqttSettings.MQTT_PROTOCOL)
-    public void setProtocol(String protocol) {
-        this.protocol = protocol;
-        requestConnecting();
-    }
-
-    @Setting(MqttSettings.MQTT_USERNAME)
-    public void setUsername(String username) {
-        this.username = username;
-        requestConnecting();
-    }
-
-    @Setting(MqttSettings.MQTT_PASSWORD)
-    public void setPassword(String password) {
-        this.password = password;
-        requestConnecting();
-    }
-
-    @Setting(MqttSettings.MQTT_ACTIVE)
-    public void setActive(boolean active) {
-        this.isActive = active;
-        if (isActive) {
-            requestConnecting();
-        } else {
-            cleanup();
-        }
-
-    }
-
-    /**
-     * @return the topic
-     */
-    public String getTopic() {
-        return topic;
-    }
-
-    /**
-     * @return the host
-     */
-    public String getHost() {
-        return host;
-    }
-
-    /**
-     * @return the port
-     */
-    public String getPort() {
-        return port;
-    }
-
-    public String getDecoder() {
+    public MqttDecoder getDecoder() {
         return decoder;
     }
 
-    public String getProtocol() {
-        return protocol;
+    public void setDecoder(MqttDecoder decoder) {
+        this.decoder = decoder;
     }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public boolean isActive() {
-        return isActive;
-    }
-
 }
